5caa1e3dedc3c73ed6e3c3d181cfd8a7c7a0320d
fahad5682431 <fahad@inverseai.com>
2023-11-08 09:37:11 +0600

    Built a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import axios from 'axios'
-interface Prop{
-  id:number;
-  name:string;
+import { useEffect, useState } from "react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import "./App.css";
+import axios from "axios";
+interface Prop {
+  id: number;
+  name: string;
 }
 function App() {
-  const [User,SetUser]=useState<Prop[]>([])
+  const [User, SetUser] = useState<Prop[]>([]);
 
-  useEffect(()=>{
-    axios.get<Prop[]>('https://jsonplaceholder.typicode.com/users').then(res=>SetUser(res.data))
-  })
-  
+  useEffect(() => {
+    axios
+      .get<Prop[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => SetUser(res.data));
+  });
+
   return (
-    <div>
-      <ul>
-        {
-          User.map(
-            (val)=><li>{val.id} {val.name}</li>
-          )
-        }
-      </ul>
-    </div>
-  )
+    <Grid
+      templateAreas={{
+        base : `"nav" "main"`,
+        lg : `"nav nav" "aside main"`
+      }}
+      h="200px"
+      gap="1"
+      color="blackAlpha.700"
+      fontWeight="bold"
+    >
+      <GridItem area={"nav"} bg="coral">Nav</GridItem>
+      <Show above="lg">
+      <GridItem area={"aside"} bg="blue">Aside</GridItem>
+      </Show>
+      <GridItem area={"main"} bg="green">Main</GridItem>
+
+    </Grid>
+  );
 }
 
-export default App
+export default App;
